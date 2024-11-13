d5b5c5f56424b60fabc6885838f32c0715a4130f
Billy Van Ark <4hchm4xub2@snkmail.com>
2024-11-13 09:13:02 +0100

    Patch: Rich! updated barter items, b414213562

--- Barter.lua
+++ Barter.lua
@@ -18,14 +18,65 @@ _BARTER =
 	[7] = {[1]=1879206820;[2]={[1]=100;};};		-- Gold Panning
 	[8] = {[1]=1879206821;[2]={[1]=30;};};		-- Dowse
 	[9] = {[1]=1879210276;[2]={[1]=60;};};		-- Treasure Hunter's Satchel
-	[10] = {[1]=1879207875;[2]={[1]=30;};};		-- Domesticated Cave-claw
+	[10] = {[1]=1879207875;[2]={[1]=5;};};		-- Domesticated Cave-claw
 	[11] = {[1]=1879206803;[2]={[1]=150;};};	-- Treasure Hunter's Pack
 	[12] = {[1]=1879206804;[2]={[1]=200;};};	-- Jeweller's Helm
-	[13] = {[1]=1879223825;[2]={[1]=200;};};	-- Sturdy Steel Key
+	[13] = {[1]=1879352247;[2]={[1]=200;}; [3]=300; };	-- 300 Motes of Enchantment
 	[14] = {[1]=1879206818;[2]={[1]=250;};};	-- Gem Inspection
 	[15] = {[1]=1879206807;[2]={[1]=400;};};	-- Trained Cave-claw
 	[16] = {[1]=1879206697;[2]={[1]=500;};};	-- Golden Cave-claw Mask
 	[17] = {[1]=1879206819;[2]={[1]=300;};};	-- Rich!
 	[18] = {[1]=1879255355;[2]={[1]=1000;};};	-- Harvest-brew Goat
 	[19] = {[1]=1879255443;[2]={[1]=500;};};	-- Unearthed Sack
+	[20] = {[1]=1879314475;[2]={[1]=500;};}; 	-- Tome of the Treasure-seeking Cave-claw
+	[21] = {[1]=1879315057;[2]={[1]=500;};}; 	-- Tome of the Tan-back Cave-claw
+	[22] = {[1]=1879315061;[2]={[1]=500;};}; 	-- Tome of the Green-beak Cave-claw
+	[23] = {[1]=1879315064;[2]={[1]=500;};}; 	-- Tome of the Pink-beak Cave-claw
+
+	[24] = {[1]=1879315068;[2]={[1]=500;};};	-- Tome of the Black-beak Cave-claw
+	[25] = {[1]=1879315061;[2]={[1]=500;};};	-- Tome of the Green-back Cave-claw
+	[26] = {[1]=1879305771;[2]={[1]=150;};};	-- Tome of the White Chicken
+	[27] = {[1]=1879305770;[2]={[1]=225;};};	-- Tome of the Red Chicken
+	[28] = {[1]=1879305772;[2]={[1]=300;};};	-- Tome of the Dorking Chicken
+	[29] = {[1]=1879305775;[2]={[1]=375;};};	-- Tome of the Scrapper Chicken
+	[30] = {[1]=1879305774;[2]={[1]=450;};};	-- Tome of the Black-foot Chicken
+	[31] = {[1]=1879305773;[2]={[1]=525;};};	-- Tome of the Orange Wyandotte Chicken
+	[32] = {[1]=1879305713;[2]={[1]=60;};};		-- Standing Streamer
+	[33] = {[1]=1879305732;[2]={[1]=60;};};		-- Seedy Hobbit Wagon
+	[34] = {[1]=1879305714;[2]={[1]=90;};};		-- Yellow Hobbit Waggon
+	[35] = {[1]=1879305707;[2]={[1]=90;};};		-- Red Hobbit Waggon
+	[36] = {[1]=1879305717;[2]={[1]=90;};};		-- Green Hobbit Waggon
+	[37] = {[1]=1879305708;[2]={[1]=90;};};		-- Blue Hobbit Waggon
+	[38] = {[1]=1879305712;[2]={[1]=30;};};		-- Broken Hobbit Waggon
+	[39] = {[1]=1879305869;[2]={[1]=10;};};		-- Oliphaunt Ear Recipe
+	[40] = {[1]=1879305870;[2]={[1]=10;};};		-- Roasted Coney on a Stick Recipe
+	[41] = {[1]=1879305864;[2]={[1]=10;};};		-- Chicken Wing Recipe
+	[42] = {[1]=1879305865;[2]={[1]=10;};};		-- Sticky Apple Recipe
+	[43] = {[1]=1879305868;[2]={[1]=10;};};		-- Took in the Hole Recipe
+
+	[44] = {[1]=1879305866;[2]={[1]=2;};};		-- Flagon of 'Shire-Honey' Mead Recipe
+	[45] = {[1]=1879305863;[2]={[1]=2;};};		-- Glass of Dark Stout Recipe
+	[46] = {[1]=1879305867;[2]={[1]=2;};};		-- Bottle of 'Sticky Apple' Cider Recipe
+	[47] = {[1]=1879305936;[2]={[1]=5;};};		-- Oliphaunt Ear Banquet Recipe
+	[48] = {[1]=1879305934;[2]={[1]=5;};};		-- Roasted Coney on a Stick Banquet Recipe
+	[49] = {[1]=1879305932;[2]={[1]=5;};};		-- Chicken Wing Banquet Recipe
+	[50] = {[1]=1879305935;[2]={[1]=5;};};		-- Sticky Apple Banquet Recipe
+	[51] = {[1]=1879305933;[2]={[1]=5;};};		-- Took in the Hole Banquet Recipe
+	[52] = {[1]=1879305937;[2]={[1]=5;};};		-- Hobnanigans Banquet Recipe
+	[53] = {[1]=1879305619;[2]={[1]=5;};};		-- Facepaint Stencil - Flower
+	[54] = {[1]=1879305622;[2]={[1]=5;};};		-- Facepaint Stencil - Butterfly
+	[55] = {[1]=1879305616;[2]={[1]=5;};};		-- Facepaint Stencil - Bee
+	[56] = {[1]=1879305620;[2]={[1]=5;};};		-- Facepaint Stencil - Feather
+	[57] = {[1]=1879305618;[2]={[1]=5;};};		-- Facepaint Stencil - Star
+	[58] = {[1]=1879305615;[2]={[1]=5;};};		-- Facepaint Stencil - Sword
+	[59] = {[1]=1879305621;[2]={[1]=5;};};		-- Facepaint Stencil - Bow
+	[60] = {[1]=1879305617;[2]={[1]=5;};};		-- Facepaint Stencil - Pipe
+	[61] = {[1]=1879305705;[2]={[1]=5;};};		-- White Carrying Chicken
+	[62] = {[1]=1879305701;[2]={[1]=6;};};		-- Red Carrying Chicken
+	[63] = {[1]=1879305706;[2]={[1]=7;};};		-- Dorking Carrying Chicken
+
+	[64] = {[1]=1879305704;[2]={[1]=8;};};		-- Scrapper Carrying Chicken
+	[65] = {[1]=1879305703;[2]={[1]=9;};};		-- Black-foot Carrying Chicken
+	[66] = {[1]=1879305702;[2]={[1]=10;};};		-- Orange Wyandotte Carrying Chicken
+
 	};
