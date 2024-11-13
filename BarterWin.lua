8d5988bd196e6811ff13427a5401d1eab9813e12
Billy Van Ark <4hchm4xub2@snkmail.com>
2024-11-13 11:18:01 +0100

    Updated barter window with dividers.

--- BarterWin.lua
+++ BarterWin.lua
@@ -64,60 +64,81 @@ function RefreshBarterList()
 		local ROWHOLDER = Turbine.UI.Control();
 		ROWHOLDER:SetSize(ROWWIDTH,ROWHEIGHT);
 
-		local ROWBACK = Turbine.UI.Control();
-		ROWBACK:SetParent(ROWHOLDER);
-		ROWBACK:SetSize(ROWWIDTH,ROWHEIGHT-5);
-		ROWBACK:SetBackground(_IMAGES.BARTERITEMSBACK);
-		ROWBACK:SetBlendMode(4);
+        if (v[1] == "DIVIDER1" or v[1] == "DIVIDER2") then
+            local dividerLabel = Turbine.UI.Label();
+            dividerLabel:SetParent(ROWHOLDER);
+            dividerLabel:SetSize(ROWWIDTH, ROWHEIGHT);
 
-		local item = 0;
-		if type(v[1]) == 'string' then
-			item = NewItemInfo(v[1],true);
-		else
-			item = NewItemInfo(v[1]);
-		end
+            if (v[1] == "DIVIDER1") then
+                dividerLabel:SetFont(Turbine.UI.Lotro.Font.BookAntiquaBold22);
+            elseif (v[1] == "DIVIDER2") then
+                dividerLabel:SetFont(Turbine.UI.Lotro.Font.BookAntiqua22);
+            end
 
-		local cItemInspect = Turbine.UI.Lotro.ItemInfoControl();
-		cItemInspect:SetParent(ROWBACK);
-		cItemInspect:SetSize(36,36);
-		cItemInspect:SetPosition(1,1);
-		cItemInspect:SetItemInfo(item:GetItemInfo());
-		cItemInspect:SetQuantity(1);
+            if (v.DIVIDER_HEIGHT) then
+                ROWHOLDER:SetHeight(v.DIVIDER_HEIGHT);
+                dividerLabel:SetHeight(v.DIVIDER_HEIGHT);
+            end
 
-		if v[3] ~= nil then cItemInspect:SetQuantity(v[3]) end;
+            dividerLabel:SetFontStyle(Turbine.UI.FontStyle.Outline);
+            dividerLabel:SetTextAlignment(Turbine.UI.ContentAlignment.MiddleCenter);
+            dividerLabel:SetText(GetString(v[2]));
+        else        
+            local ROWBACK = Turbine.UI.Control();
+            ROWBACK:SetParent(ROWHOLDER);
+            ROWBACK:SetSize(ROWWIDTH,ROWHEIGHT-5);
+            ROWBACK:SetBackground(_IMAGES.BARTERITEMSBACK);
+            ROWBACK:SetBlendMode(4);
 
-		local lblItemName = Turbine.UI.Label();
-		lblItemName:SetParent(ROWBACK);
-		lblItemName:SetPosition(40,2);
-		lblItemName:SetSize(ROWWIDTH-150,36);
-		lblItemName:SetTextAlignment(Turbine.UI.ContentAlignment.MiddleLeft);
-		lblItemName:SetForeColor(_QUALITYCOLORS[item:GetItemInfo():GetQuality()]);
-		--lblItemName:SetBackColor(Turbine.UI.Color.Red);
-		lblItemName:SetFont(Turbine.UI.Lotro.Font.TrajanPro14);
-		lblItemName:SetText(item:GetItemInfo():GetName());
+            local item = 0;
+            if type(v[1]) == 'string' then
+                item = NewItemInfo(v[1],true);
+            else
+                item = NewItemInfo(v[1]);
+            end
 
-		local lstTokensNeeded = Turbine.UI.ListBox();
-		lstTokensNeeded:SetParent(ROWBACK);
-		lstTokensNeeded:SetSize(110,36);
-		lstTokensNeeded:SetPosition(ROWWIDTH-lstTokensNeeded:GetWidth(),0);
-		lstTokensNeeded:SetOrientation(Turbine.UI.Orientation.Horizontal);
-		--lstTokensNeeded:SetBackColor(Turbine.UI.Color.Red);
+            local cItemInspect = Turbine.UI.Lotro.ItemInfoControl();
+            cItemInspect:SetParent(ROWBACK);
+            cItemInspect:SetSize(36,36);
+            cItemInspect:SetPosition(1,1);
+            cItemInspect:SetItemInfo(item:GetItemInfo());
+            cItemInspect:SetQuantity(1);
 
-		if type(v[2])=='table' then
-			for k,v in pairs (v[2]) do
-				local tokenItem = NewItemInfo(_TOKENS[k]);
-				if tokenItem ~= nil then
-					local cTokenInspect = Turbine.UI.Lotro.ItemInfoControl();
-					cTokenInspect:SetParent(ROWBACK);
-					cTokenInspect:SetSize(36,36);
-					cTokenInspect:SetPosition(1,1);
-					cTokenInspect:SetItemInfo(tokenItem:GetItemInfo());
-					cTokenInspect:SetQuantity(v);
+            if v[3] ~= nil then cItemInspect:SetQuantity(v[3]) end;
 
-					lstTokensNeeded:AddItem(cTokenInspect);
-				end
-			end
-		end
+            local lblItemName = Turbine.UI.Label();
+            lblItemName:SetParent(ROWBACK);
+            lblItemName:SetPosition(40,2);
+            lblItemName:SetSize(ROWWIDTH-150,36);
+            lblItemName:SetTextAlignment(Turbine.UI.ContentAlignment.MiddleLeft);
+            lblItemName:SetForeColor(_QUALITYCOLORS[item:GetItemInfo():GetQuality()]);
+            --lblItemName:SetBackColor(Turbine.UI.Color.Red);
+            lblItemName:SetFont(Turbine.UI.Lotro.Font.TrajanPro14);
+            lblItemName:SetText(item:GetItemInfo():GetName());
+
+            local lstTokensNeeded = Turbine.UI.ListBox();
+            lstTokensNeeded:SetParent(ROWBACK);
+            lstTokensNeeded:SetSize(110,36);
+            lstTokensNeeded:SetPosition(ROWWIDTH-lstTokensNeeded:GetWidth(),0);
+            lstTokensNeeded:SetOrientation(Turbine.UI.Orientation.Horizontal);
+            --lstTokensNeeded:SetBackColor(Turbine.UI.Color.Red);
+
+            if type(v[2])=='table' then
+                for k,v in pairs (v[2]) do
+                    local tokenItem = NewItemInfo(_TOKENS[k]);
+                    if tokenItem ~= nil then
+                        local cTokenInspect = Turbine.UI.Lotro.ItemInfoControl();
+                        cTokenInspect:SetParent(ROWBACK);
+                        cTokenInspect:SetSize(36,36);
+                        cTokenInspect:SetPosition(1,1);
+                        cTokenInspect:SetItemInfo(tokenItem:GetItemInfo());
+                        cTokenInspect:SetQuantity(v);
+
+                        lstTokensNeeded:AddItem(cTokenInspect);
+                    end
+                end
+            end
+        end
 
 		lstBarterItems:AddItem(ROWHOLDER);
 	end
